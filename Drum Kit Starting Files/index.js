4de8df59407358d55f722ccb5a15416201dd5082
Xiaoyue ZHANG <zhangxiaoyue2022@163.com>
2023-06-02 21:40:18 +0100

    drum

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,63 @@
+//detect the click
+var numberOfDrum = document.querySelectorAll(".drum").length;
+
+for(var i=0; i<numberOfDrum; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+        var buttonOfHTML=this.innerHTML;
+        madeSound(buttonOfHTML);
+        buttonAnimation(buttonOfHTML);
+        
+    });
+}
+
+//detect the keypress
+document.addEventListener("keypress",function(event){
+    var keyPressLetter=event.key
+    madeSound(keyPressLetter);
+    buttonAnimation(keyPressLetter);
+})
+
+function madeSound(key){
+    switch (key) {
+        case "w":
+            var tom1=new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case "a":
+            var tom2=new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case "s":
+            var tom3=new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case "d":
+            var tom4=new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        case "j":
+            var snare=new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case "k":
+            var crash=new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case "l":
+            var kick=new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+            
+        default:console.log("oh no");
+            break;
+    }
+}
+
+//Button Animation
+function buttonAnimation(currentkey){
+    var activeButton=document.querySelector("."+currentkey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+}
